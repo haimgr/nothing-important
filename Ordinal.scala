9ce005fa535ee1223843d60180dca48cb6154d4a
haimgr <haim763@gmail.com>
2016-09-07 18:35:02 +0300

    Ordinal.scala

--- Ordinal.scala
+++ Ordinal.scala
@@ -6,24 +6,31 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by haim7 on 07 ספטמבר 2016.
   */
-class Ordinal[T <: Ordinal[T]](creator: OrdinalCreator[T]) {
+class Ordinal[T <: Ordinal[T]](creator: OrdinalCreator[T]) extends Ordered[T] {
 
   private val self = this.asInstanceOf[T]
 
   val id: Int = creator.add(self)
 
-  def +(that: T): Set[T] = Set[T](self, that)
+  def +(that: T): Set[T] = Set(self, that)
 
   override def toString: String = getClass.getSimpleName + "(" + id + ")"
+
+  override def compare(that: T): Int = creator.compare(self, that)
 }
 
-trait OrdinalCreator[T <: Ordinal[T]] {
-  private val _values = new ArrayBuffer[T]
+trait OrdinalCreator[T <: Ordinal[T]] extends Ordering[T] {
+
+  private var _values = new ArrayBuffer[T]
+
   def add(ordinal: T): Int = {
     val ret = _values.size
     _values += ordinal
     ret
   }
-  def values: Seq[T] = {val ret = _values.toList; _values == null; ret}
+
+  lazy val values: Seq[T] = {val ret = _values.toList; _values = null; ret}
+
+  override def compare(x: T, y: T): Int = x.id - y.id
 
 }
